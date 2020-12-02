16c56dbe069c647041ac7ddc0dc66d3228a6a67b
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-12-02 09:54:32 +0800

    console logged all 54 countries data

--- src/app/components/countries.component.ts
+++ src/app/components/countries.component.ts
@@ -11,10 +11,21 @@ export class CountriesComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    // top headline countries list taken from https://newsapi.org/docs/endpoints/top-headlines
     const countries = "ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za"
     const allCountries = countries.split(" ").join(";")
-    console.log('allCountries ---> ', allCountries)
- 
+    // console.log('allCountries ---> ', allCountries)
+
+    // https://restcountries.eu/rest/v2/alpha?codes=ae;il;etc..
+    const base_url = 'https://restcountries.eu/rest/v2/alpha'
+    const countriesParams = new HttpParams().set('codes', allCountries)
+
+    this.http.get<any>(base_url, { params: countriesParams })
+      .toPromise()
+      .then(response => {
+        const results = response
+        console.log(results)
+      })
 
   }
 
